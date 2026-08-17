c048afd8575d8cc7ae2ac3408f5a326c5a31fd77
shayh20 <shayh20@gmail.com>
2026-08-17 12:27:12 +0300

    feat(funday): render activity cards from data module

--- funday/public/js/main.js
+++ funday/public/js/main.js
@@ -1,6 +1,8 @@
 import { initScrollReveal, initScrollCue } from './animations.js';
+import { renderActivityCards } from './activities-view.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  renderActivityCards();
   initScrollReveal();
   initScrollCue();
 });
