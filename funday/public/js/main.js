8dda8c9ba1f98155ffbdecde6d48f49d604271fe
shayh20 <shayh20@gmail.com>
2026-08-19 12:20:21 +0300

    feat(funday): add a fading sage-green leaf trail that follows the cursor

--- funday/public/js/main.js
+++ funday/public/js/main.js
@@ -1,8 +1,10 @@
 import { initScrollReveal, initScrollCue } from './animations.js';
 import { renderActivityCards } from './activities-view.js';
+import { initCursorTrail } from './cursor-trail.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   renderActivityCards();
+  initCursorTrail();
   initScrollReveal();
   initScrollCue();
 });
